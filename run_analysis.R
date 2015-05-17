1969b92bf395f73e0b4f501d08d05b6cc481969c
gcalderonmeza <gcalderonmeza@gmail.com>
2015-05-17 08:14:13 -0700

    Improvements on .R and output files

--- run_analysis.R
+++ run_analysis.R
@@ -6,13 +6,13 @@ if (file.exists("./UCI HAR Dataset")) {
   # Load data from the given files
   featureNames <- read.table("./UCI HAR Dataset/features.txt")
   dataFrame <- read.table("./UCI HAR Dataset/train/X_train.txt", col.names = featureNames[,2])
-  activities <- read.table("./UCI HAR Dataset/train/Y_train.txt", col.names = c("Activity"))
+  activities <- read.table("./UCI HAR Dataset/train/Y_train.txt", col.names = c("ActivityId"))
   testData <- read.table("./UCI HAR Dataset/test/X_test.txt", col.names = featureNames[,2])
-  activitiesTest <- read.table("./UCI HAR Dataset/test/Y_test.txt", col.names = c("Activity"))
+  activitiesTest <- read.table("./UCI HAR Dataset/test/Y_test.txt", col.names = c("ActivityId"))
   subjects <- read.table("./UCI HAR Dataset/train/subject_train.txt", col.names = c("Subject"))
   subjectsTest <- read.table("./UCI HAR Dataset/test/subject_test.txt", col.names = c("Subject"))
   
-  # Mege subjects (test and train)
+  # Merge subjects (test and train)
   subjects <- rbind(subjects, subjectsTest)
 
   # Merge activities (test and train)
@@ -28,13 +28,12 @@ if (file.exists("./UCI HAR Dataset")) {
   rm(activities)
   rm(activitiesTest)
 
-  # Extract the columns fr mean and std for all measurements
-  dataFrameSelected <- dataFrame[sort(c(1, grep("-mean", as.character(featureNames$V2)) + 1, grep("-std", as.character(featureNames$V2)) + 1))]
+  # Extract the columns for mean and std for all measurements
+  dataFrameSelected <- dataFrame[sort(c(1, 2, grep("-mean", as.character(featureNames$V2)) + 2, grep("-std", as.character(featureNames$V2)) + 2))]
   
-  # Tidy the data (I do not see a link to activity in the data)
   dataTidy <- melt(dataFrameSelected, id.vars= c("Subject", "Activity"))
 
-  dataSummary <- dcast(dataTidy, Subject ~ variable, fun.aggregate = mean)
+  dataSummary <- dcast(dataTidy, Subject + Activity ~ variable, fun.aggregate = mean)
   
   # write the output
   write.table(dataSummary, file="./output.txt", row.name=FALSE)
