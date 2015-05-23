50b24f348fc4331ff3e02db2075b596dd0ed77a8
gcalderonmeza <gcalderonmeza@gmail.com>
2015-05-22 17:40:43 -0700

    Adding activity labels to the output

--- run_analysis.R
+++ run_analysis.R
@@ -11,12 +11,13 @@ if (file.exists("./UCI HAR Dataset")) {
   activitiesTest <- read.table("./UCI HAR Dataset/test/Y_test.txt", col.names = c("ActivityId"))
   subjects <- read.table("./UCI HAR Dataset/train/subject_train.txt", col.names = c("Subject"))
   subjectsTest <- read.table("./UCI HAR Dataset/test/subject_test.txt", col.names = c("Subject"))
+  activityLabels <- read.table("./UCI HAR Dataset/activity_labels.txt", col.names = c("ActivityId", "Activity"))
   
   # Merge subjects (test and train)
   subjects <- rbind(subjects, subjectsTest)
 
   # Merge activities (test and train)
-  activities <- rbind(activities, activitiesTest)
+  activities <- merge(rbind(activities, activitiesTest), activityLabels)
   
   # Create the single Frame
   dataFrame <- cbind(subjects, activities, rbind(dataFrame, testData))
@@ -27,9 +28,10 @@ if (file.exists("./UCI HAR Dataset")) {
   rm(subjects)
   rm(activities)
   rm(activitiesTest)
+  rm(activityLabels)
 
   # Extract the columns for mean and std for all measurements
-  dataFrameSelected <- dataFrame[sort(c(1, 2, grep("-mean", as.character(featureNames$V2)) + 2, grep("-std", as.character(featureNames$V2)) + 2))]
+  dataFrameSelected <- dataFrame[sort(c(1, 3, grep("-mean", as.character(featureNames$V2)) + 3, grep("-std", as.character(featureNames$V2)) + 3))]
   
   dataTidy <- melt(dataFrameSelected, id.vars= c("Subject", "Activity"))
 
